76f724850271d0c589412598bfb02e55f0b88ca6
Ilgar Mashayev <ilgar@mashayev.com>
2025-02-26 11:06:27 +0100

    Initial

---
+++ content/figure.md
@@ -0,0 +1,5 @@
+Title: Figure
+Date: 2010-12-03 10:20
+Category: Figure
+
+Figure images.
---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+AUTHOR = 'Me'
+SITENAME = 'Quick Pose'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = False
+
+TAGS_SAVE_AS = ''
+TAG_SAVE_AS = ''
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
